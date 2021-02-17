380f65bc05ef27a4196f5621ff8cceceba823687
llleyelll <chida.fua@gmail.com>
2021-02-17 18:01:14 +0700

    add issue and unstake token in contract

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -7,6 +7,7 @@ contract TokenFarm {
     string public name = "Dapp Token Farm";
     DappToken public dappToken;
     DaiToken public daiToken;
+    address public owner;
 
     address[] public stakers;
     mapping(address => uint) public stakingBalance;
@@ -16,6 +17,7 @@ contract TokenFarm {
     constructor(DappToken _dappToken, DaiToken _daiToken) public {
         dappToken = _dappToken;
         daiToken = _daiToken;
+        owner = msg.sender;
     }
 
     function stakeTokens(uint _amount) public {
@@ -29,5 +31,29 @@ contract TokenFarm {
 
         isStaking[msg.sender] = true;
         hasStaked[msg.sender] = true;
+    }
+
+    function unstakeTokens() public {
+        uint balance = stakingBalance[msg.sender];
+
+        require(balance > 0, "staking balance cannot be 0");
+
+        daiToken.transfer(msg.sender, balance);
+
+        stakingBalance[msg.sender] = 0;
+
+        isStaking[msg.sender] = false;
+    }
+
+    function issueTokens() public {
+        require(msg.sender == owner, "caller must be the owner");
+
+        for (uint i=0; i < stakers.length; i++) {
+            address recipient = stakers[i];
+            uint balance = stakingBalance[recipient];
+            if(balance > 0) {
+                dappToken.transfer(recipient, balance);
+            }
+        }
     }
 }
